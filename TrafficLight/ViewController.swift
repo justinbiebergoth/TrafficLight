9bd84c36fb6bb8c66ce71dc2263277c1c9a9cb51
justinbiebergoth <hiirari666@gmail.com>
2022-09-18 22:07:11 +0300

    traffic light v1.0.0

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum CurrentLightState {
+        case off, red, yellow, green
+    }
+    
+    var currentLightState: CurrentLightState = .off {
+        didSet {
+            switch currentLightState {
+            case .off:
+                redLight.alpha = 0.3
+                yellowLight.alpha = 0.3
+                greenLight.alpha = 0.3
+                changeColourButton.setTitle("START", for: .normal)
+            case .red:
+                redLight.alpha = 1
+                yellowLight.alpha = 0.3
+                greenLight.alpha = 0.3
+                
+            case .yellow:
+                yellowLight.alpha = 1
+                greenLight.alpha = 0.3
+                redLight.alpha = 0.3
+            case .green:
+                greenLight.alpha = 1
+                redLight.alpha = 0.3
+                yellowLight.alpha = 0.3
+            }
+        }
+    }
+    
+    @IBOutlet var changeColourButton: UIButton!
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeColourButton.layer.cornerRadius = 20
+        currentLightState = .off
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "kaworu")
+        backgroundImage.contentMode = .scaleAspectFill
+        view.insertSubview(backgroundImage, at: 0)
+        
+        viewDidLayoutSubview()
     }
+    
+    func viewDidLayoutSubview() {
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
+    }
+   
 
+    @IBAction func changeColourButtonTapped(sender: UIButton) {
+        changeColourButton.setTitle("NEXT", for: .normal)
+        switch currentLightState {
+        case .off:
+            currentLightState = .red
+        case .red:
+            currentLightState = .yellow
+        case .yellow:
+            currentLightState = .green
+        case .green:
+            currentLightState = .off
+        }
 
-}
+    }
+    
+    }
+    
+
 
